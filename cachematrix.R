3dc318fbe28d5dbad868047cbff30631e94d2d0e
Kunal Bajpai <kunalkb20@gmail.com>
2015-04-15 18:46:13 +0530

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+#Creates a list with methods to get and set data and inverted matrix in inv
 makeCacheMatrix <- function(x = matrix()) {
 	inv <- NULL
 	set <- function(y) {
@@ -17,7 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+#Checks if value is cached and returns it if it is else computes the invert, caches it and returns
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv <- x$getinv()
